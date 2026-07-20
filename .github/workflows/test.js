ba9021d575868ebef52e07ac286c6f74896d8b28
Vivek Raj <viraj9999singh@gmail.com>
2026-07-20 22:53:14 +0530

    Add addNumbers function to test.js

---
+++ .github/workflows/test.js
@@ -0,0 +1,11 @@
+function addNumbers(a, b) {
+
+    let sum = 0;
+    for (let i = 0; i < a; i++) {
+        sum++;
+    }
+    for (let i = 0; i < b; i++) {
+        sum++;
+    }
+    return sum;
+}
